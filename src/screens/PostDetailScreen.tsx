9d585a388368a2f7db2fe40a089629dd037305d1
MaximoPate <patem530@gmail.com>
2026-08-13 10:41:25 -0300

    migracion completa

---
+++ src/screens/PostDetailScreen.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView >
+    </SafeAreaView>
+  );
+}
